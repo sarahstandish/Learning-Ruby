53aba268683f22e2226f16c8c3b7b0e02bd68eba
sarahstandish <z.sisyphusrocks@gmail.com>
2019-01-22 09:24:55 -0800

    reorganizing

--- ex37_practice.rb
+++ ex37_practice.rb
@@ -5,33 +5,18 @@
 # and in previous exercises
 
 class Student
-    def initialize(name, detention_status, house)
+    def initialize(name)
         @name = name
-        @detention_status = detention_status
-        @house = house
+        @detention_status = false
+        @house = nil
     end
 
-    def get_player_name #read the attribute
-        @name
-    end
+    attr_reader :name
+    attr_accessor :detention_status
+    attr_accessor :house
 
-    def change_detention_status=(detention_status) #using this method we can change the attribute
-        @detention_status = detention_status
-    end
 
-    def get_detention_status #read the attribute
-        @detention_status
-    end
 
-    def set_house=(house)
-        @house = house
-    end
-
-    def get_house
-        @house
-    end
-
-    alias name get_player_name #practice using alias
 end
 
 def get_player_name
@@ -42,7 +27,7 @@ end
 
 
 def choose_to_go_in(player)
-    puts "\n\n\"Welcome, #{player.get_player_name},\" Professort McGonagal greets you."
+    puts "\n\n\"Welcome, #{player.name},\" Professort McGonagal greets you."
     
     puts "\n\nThe Great Hall is in front of you.  Do you go in?"
     choice = gets.chomp
@@ -57,7 +42,7 @@ def choose_to_go_in(player)
         puts "\n\nYou enter the Great Hall."
     when /n/i #matches the character n in any case
         puts "\n\nProfessor McGonagal catches you walking away from the Great Hall. She makes you go in anyway, but gives you detention to be served after the sorting ceremony."
-        player.change_detention_status = true
+        player.detention_status = true
     end
     
 end
@@ -65,13 +50,13 @@ end
 def great_hall(player)
     puts "\n\nIn the Great Hall, you line up with the other first years at the back."
 
-    if player.get_detention_status == true
+    if player.detention_status == true
         puts "\n\nAnother first-year whispers to you: \"Wow, I can't believe you already got detention!\""
-    elsif player.get_detention_status == false
+    elsif player.detention_status == false
         puts "\n\nAnother first-year whispers to you: \"Did you hear that Billingsly didn't come into the Great Hall right away and he already got detention?\'"
     end
 
-    puts "\n\nOne by one, Professor McGonagal calls students to the front of the Great Hall to be sorted. Finally, you hear your name echoing from the stone walls:\n\n\"#{player.get_player_name.upcase}!\"\n\nIt's time.  You approach the sorting hat and place it on your head."
+    puts "\n\nOne by one, Professor McGonagal calls students to the front of the Great Hall to be sorted. Finally, you hear your name echoing from the stone walls:\n\n\"#{player.name.upcase}!\"\n\nIt's time.  You approach the sorting hat and place it on your head."
 
 end
 
@@ -79,9 +64,9 @@ def sorting_hat(player)
 
     houses = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
 
-    print "\n\nYou hear a low voice, almost muttering to itself...\n\n\"Well, #{player.get_player_name}\" it says...  "
+    print "\n\nYou hear a low voice, almost muttering to itself...\n\n\"Well, #{player.name}\" it says...  "
 
-    case player.get_detention_status
+    case player.detention_status
     when true
         puts "\"Detention already...my, my...you have been busy...it seems you would be a good fit for Gryffindor, or perhaps Slytherin...the brave and the ambitious must be ready to break the rules. Don't you agree?\""
         agree = gets.chomp
@@ -122,25 +107,47 @@ def sorting_hat(player)
     end
 
     if houses.include?(hope)
-        player.set_house = hope
-        puts "\n\n\"An easy case, then...better be #{player.get_house.upcase}!!!!\""
+        player.house = hope
+        puts "\n\n\"An easy case, then...better be #{player.house.upcase}!!!!\""
     else
-        player.set_house = houses[rand(1)]
-        puts "\n\n\"Not sure I agree...I'm placing you in...#{player.get_house.upcase}!!!!\""
+        player.house = houses[rand(1)]
+        puts "\n\n\"Not sure I agree...I'm placing you in...#{player.house.upcase}!!!!\""
     end
 
-    puts " The hat shouted the last word out loud. As the students in the great hall clap and cheer politely, you walk to the #{player.get_house} table and sit down."
+    puts " The hat shouted the last word out loud. As the students in the great hall clap and cheer politely, you walk to the #{player.house} table and sit down."
 
+end
+
+def new_friend
+
+    possible_friends = ["Thu", "Ahmed", "Selina", "Wilhelmina"]
+
+    puts "You look around and notice some students you recognize from the Hogwards Express sitting around you: #{possible_friends.join(", ")} ...which do you talk with?\""
+    friend = gets.chomp
+    unless possible_friends.include?(friend.capitalize)
+        puts "That person isn't at your table."
+        friend = gets.chomp
+    end
+    return friend.capitalize
+end
+
+def meet_friend(friend)
+    puts "#{friend.name} turns to you and says, \"Hi, I'm #{friend.name}. I'm in #{friend.house} too.\""
 end
 
 BEGIN { puts "Welcome to Hogwarts School of Witchcraft and Wizardry!"}
 
-player = Student.new(get_player_name, false, nil)
+player = Student.new(get_player_name)
 
 choose_to_go_in(player)
 
 great_hall(player)
 
 sorting_hat(player)
+
+friend = Student.new(new_friend)
+friend.house = player.house
+
+meet_friend(friend)
 
 END { puts "\n\nThanks for visiting Hogwarts!" }
